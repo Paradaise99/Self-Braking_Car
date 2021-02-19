1db63141aa99b3b67e07530c4485d5224dc17f99
Paradaise99 <44893954+Paradaise99@users.noreply.github.com>
2021-02-19 21:53:10 +0000

    Code that open the webcam

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello Word!')
+import cv2
+
+mac_cam = cv2.VideoCapture(0)
+
+while True:
+    retrieve, frames = mac_cam.read()
+    cv2.imshow('Try2Catch', frames)
+
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
