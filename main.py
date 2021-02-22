e11709c07b2673d38eacfc2c4332834a1920a17f
Paradaise99 <44893954+Paradaise99@users.noreply.github.com>
2021-02-22 15:52:15 +0000

    Some comments created

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import cv2
 
+# Create the variable called mac and video capture using the main camera of the laptop
 mac_cam = cv2.VideoCapture(0)
 
+#While loop that will run the actual camera capture 
 while True:
+    # Capture all the frames
     retrieve, frames = mac_cam.read()
-    cv2.imshow('Try2Catch', frames)
+    cv2.imshow('Self-Braking System', frames)
 
+    # To be bale to break the code
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
