499d66033c14b402beef8c6e2691c0f06705d88a
Paradaise99 <44893954+Paradaise99@users.noreply.github.com>
2021-02-22 16:29:59 +0000

    Created the detection of the eye

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 # Create the variable called mac and video capture using the main camera of the laptop
 mac_cam = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-#eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 #mouth_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_mouth.xml)
 
 #While loop that will run the actual camera capture 
@@ -13,10 +13,18 @@ while True:
 
     gray = cv2.cvtColor(frames, cv2.COLOR_BGRA2GRAY)
 
-    #detect_eyes = eye_cascade.detectMultiScale(gray, 1.1, 4)
+    
     detect_faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y,w,h) in detect_faces:
         cv2.rectangle(frames, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frames[y:y+h, x:x+w]
+        
+        detect_eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in detect_eyes:
+            cv2.rectangle(roi_color,(ex, ey), (ex+ew,ey+eh), (0, 255, 0), 2)
+
+
     cv2.imshow('Self-Braking System', frames)
 
     # To be bale to break the code
